f997cc78dceb30d344e05fca8806df043dbe62e9
gomezerik <80597948+gomezerik@users.noreply.github.com>
2021-09-24 20:37:40 -0300

    Add files via upload

---
+++ TP 1/app.js
@@ -0,0 +1,8 @@
+let nombre = prompt ('Escribí tu nombre y apellido')
+
+let num1 = prompt('Mandame tu año de nacimiento. Lo voy a multiplicar por 7 porque se me dio la gana')
+let num2 = Number(num1)
+
+console.log(num2 * 7)
+
+alert(`El año de nacimiento de ${nombre} es ${num2}, y el resultado multiplicado por 7 es ${num2 * 7}. ¡Felicidades! Ojalá te sirva para algo 🦄`)
