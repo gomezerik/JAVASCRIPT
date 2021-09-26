291f9ab705e633409b8cfdffdbf3c0b9dc52909d
gomezerik <80597948+gomezerik@users.noreply.github.com>
2021-09-26 16:01:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// Solicitamos un valor al usuario
+let Amigo1 = prompt("Ingresar el nombre de un amigx 🙋🏽‍♂️");
+let Amigo2 = prompt("Ingresar el nombre de un segundo amigx 💁🏽‍♂️");
+let Amigo3 = prompt("Ingresar el nombre de un tercer amigx 🤷🏽‍♂️");
+let Amigo4 = prompt("Escribe ESC en mayúsculas, por favor 🙏🏾✨🌈");
+
+
+while(Amigo1 != "ESC" && Amigo2 != "ESC" && Amigo3 != "ESC" ){
+    alert( Amigo1 + " te debe una pizza 🍕, " + Amigo2 + " te debe tres porciones de cheesecake 🍰 y, " + Amigo3 + " te debe $1000 por todos los consejos que le diste 😂💸.");
+}
+
